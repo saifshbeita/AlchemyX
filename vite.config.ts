628cd07dfc61480be87e2d11bdc5229b6284aed2
Saif Shbeita <saif.shbita@gmail.com>
2026-07-11 06:03:19 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-  // Pull the required variable out of the loaded environment config
+  
   const env = loadEnv(mode, '.', '');
   const { GEMINI_API_KEY } = env;
 
